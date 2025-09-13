a6a760a0b4edc232d43f614d9d8a8177e63be841
AL-Hourani <jaafar.alhorani.02@gmail.com>
2025-09-13 02:20:40 -0700

    ficc

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+# main.py
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import requests
-import os
+from typing import Optional
+import torch
+from model.generate_answer import safe_generate_answer, allowed_keywords
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-
-# my model in hugging face ....... 
-
-MODEL_HF = "jaafar-ai/miniChat"  
-API_URL = f"https://api-inference.huggingface.co/models/{MODEL_HF}"
-
-HF_TOKEN = os.getenv("HF_TOKEN")
-headers = {"Authorization": f"Bearer {HF_TOKEN}"}
+MODEL_HF = "jaafar-ai/miniChat"
+model: Optional[AutoModelForSeq2SeqLM] = None
+tokenizer: Optional[AutoTokenizer] = None
 
 app = FastAPI(title="Mini Chat API")
 
@@ -20,20 +18,15 @@ class QuestionRequest(BaseModel):
 class AnswerResponse(BaseModel):
     answer: str
 
-def query_hf_api(question: str):
-    payload = {"inputs": question}
-    response = requests.post(API_URL, headers=headers, json=payload)
-    response.raise_for_status()
-    result = response.json()
- 
-    if isinstance(result, list) and "generated_text" in result[0]:
-        return result[0]["generated_text"]
-    return str(result)
-
 @app.post("/ask", response_model=AnswerResponse)
 async def ask_question(request: QuestionRequest):
+    global model, tokenizer
     try:
-        answer = query_hf_api(request.question)
+        if model is None or tokenizer is None:
+            model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_HF , torch_dtype=torch.float16)
+            tokenizer = AutoTokenizer.from_pretrained(MODEL_HF)
+
+        answer = safe_generate_answer(model, tokenizer, request.question, allowed_keywords)
         return AnswerResponse(answer=answer)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
